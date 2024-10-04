14abe6d386d3a2576a576bd7238864489d0f763c
Phillip <phillipheels90@gmail.com>
2024-10-04 15:02:28 -0600

    Clean up - Initial

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 
-trait Window<T, I> {
-    fn window(num: T) -> I;
+trait Window<T> {
+    fn window(num: usize) -> T;
 }
 
 trait CosineWindow<T> {
@@ -9,19 +9,19 @@ trait CosineWindow<T> {
 
 pub const PI: f32 = 3.14159265358979323846264338327950288_f32;
 
-struct Hann; 
+struct Hann {}
 
 impl CosineWindow<f32> for Hann {
-    fn cosine_window(mut window: &mut Vec<f32>) {
+    fn cosine_window(window: &mut Vec<f32>) {
         let size: usize = window.len();
         window.iter_mut().for_each(|x| {
-            let multiplier = PI * 2 as f32 * *x as f32 / (size as f32 - 1 as f32);
+            let multiplier = PI * 2_f32 * *x / (size as f32 - 1_f32);
             *x = 0.5 - 0.5 * multiplier.cos();
         });
     }
 }
 
-impl Window<usize, Vec<f32>> for Hann {
+impl Window<Vec<f32>> for Hann {
     fn window(num: usize) -> Vec<f32> {
         let mut window: Vec<_> = (0u8 .. num as u8).map(f32::from).collect();
         Self::cosine_window(&mut window);
@@ -36,6 +36,7 @@ mod tests {
     #[test]
     fn test_hanning_window() {
         let window = Hann::window(51);
+        println!("{:?}", window);
 
     }
 }
