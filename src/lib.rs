57e3f529a71229c102af95d73350c3557a64789a
Phillip <phillipheels90@gmail.com>
2024-10-04 20:17:47 -0600

    Adding Hamming Window

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,15 @@ trait Window<T> {
 }
 
 trait CosineWindow<T> {
-    fn cosine_window(v: &mut Vec<T>);
+    fn cosine_window(window: &mut Vec<T>);
 }
 
 pub const PI: f32 = 3.14159265358979323846264338327950288_f32;
 
-struct Hann {}
+struct Hanning {}
+struct Hamming {}
 
-impl CosineWindow<f32> for Hann {
+impl CosineWindow<f32> for Hanning {
     fn cosine_window(window: &mut Vec<f32>) {
         let size: usize = window.len();
         window.iter_mut().for_each(|x| {
@@ -21,11 +22,29 @@ impl CosineWindow<f32> for Hann {
     }
 }
 
-impl Window<Vec<f32>> for Hann {
+impl CosineWindow<f32> for Hamming {
+    fn cosine_window(window: &mut Vec<f32>) {
+        let size: usize = window.len();
+        window.iter_mut().for_each(|x| {
+            let multipler = PI * 2_f32 * *x / (size as f32 - 1_f32);
+            *x = 0.54 - 0.46 * multipler.cos();
+        });
+    }
+}
+
+impl Window<Vec<f32>> for Hanning {
     fn window(num: usize) -> Vec<f32> {
         let mut window: Vec<_> = (0u8 .. num as u8).map(f32::from).collect();
         Self::cosine_window(&mut window);
         window  
+    }
+}
+
+impl Window<Vec<f32>> for Hamming {
+    fn window(num: usize) -> Vec<f32> {
+        let mut window: Vec<_> = (0u8..num as u8).map(f32::from).collect();
+        Self::cosine_window(&mut window);
+        window
     }
 }
 
@@ -35,8 +54,13 @@ mod tests {
 
     #[test]
     fn test_hanning_window() {
-        let window = Hann::window(51);
+        let window = Hanning::window(51);
         println!("{:?}", window);
+    }
 
+    #[test]
+    fn test_hamming_window() {
+        let window = Hamming::window(51);
+        println!("{:?}", window);
     }
 }
