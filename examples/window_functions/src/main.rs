f10be4ea383adc8b6726f8200c9c02ed73ecb39c
Phillip <phillipheels90@gmail.com>
2024-10-05 19:57:54 -0600

    Small change to update examples

--- examples/window_functions/src/main.rs
+++ examples/window_functions/src/main.rs
@@ -5,6 +5,7 @@ enum WindowFunction {
     HAMMING,
     HANNING,
     BARTLETT,
+    BLACKMAN,
 }
 
 fn get_window(window: WindowFunction, size: usize) -> Vec<f32> {
@@ -12,10 +13,11 @@ fn get_window(window: WindowFunction, size: usize) -> Vec<f32> {
         WindowFunction::HAMMING => window_functions::Hamming::window(size),
         WindowFunction::HANNING => window_functions::Hanning::window(size),
         WindowFunction::BARTLETT => window_functions::Bartlett::window(size),
+        WindowFunction::BLACKMAN => window_functions::Blackman::window(size),
     }
 }
 
 
 fn main() {
-    let window = get_window(WindowFunction::BARTLETT, 51);
+    let window = get_window(WindowFunction::BLACKMAN, 51);
 }
