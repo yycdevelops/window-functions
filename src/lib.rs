c03c304e6fa6fdfc1d20881b3278062805e749dd
Phillip <phillipheels90@gmail.com>
2024-10-05 19:55:28 -0600

    Adding another window function

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,8 @@ trait CosineWindow<T> {
 pub struct Hanning {}
 pub struct Hamming {}
 pub struct Bartlett {}
+pub struct Blackman {}
+
 
 impl CosineWindow<f32> for Hanning {
     fn cosine_window(window: &mut Vec<f32>) {
@@ -28,6 +30,21 @@ impl CosineWindow<f32> for Hamming {
         window.iter_mut().for_each(|x| {
             let multipler = f32::consts::PI * 2_f32 * *x / (size as f32 - 1_f32);
             *x = 0.54 - 0.46 * multipler.cos();
+        });
+    }
+}
+
+impl CosineWindow<f32> for Blackman {
+    fn cosine_window(window: &mut Vec<f32>) {
+        let co1 = 0.42; 
+        let co2 = 0.5; 
+        let co3 = 0.08;
+        let size: usize = window.len();
+        
+        window.iter_mut().for_each(|x| {
+            let multiplier_left = f32::consts::PI * 2_f32 * *x / size as f32;
+            let multiplier_right = f32::consts::PI * 4_f32 * *x / size as f32;
+            *x = co1 - co2 * multiplier_left.cos() + co3 * multiplier_right.cos();
         });
     }
 }
@@ -58,6 +75,14 @@ impl Window<Vec<f32>> for Bartlett {
     }
 }
 
+impl Window<Vec<f32>> for Blackman {
+    fn window(num: usize) -> Vec<f32> {
+        let mut window_blackman: Vec<_> = (0u8..num as u8).map(f32::from).collect();
+        Self::cosine_window(&mut window_blackman);
+        window_blackman
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -78,6 +103,11 @@ mod tests {
     fn test_barlet() {
         let window = Bartlett::window(51);
         println!("{:?}", window);
+    }
 
+    #[test]
+    fn test_blackman() {
+        let window = Blackman::window(51);
+        println!("{:?}", window);
     }
 }
